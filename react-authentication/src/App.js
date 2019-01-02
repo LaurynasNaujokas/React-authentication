3d970d4b0aa7859e83d87afa94238450b4b65ddc
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-02 21:52:14 +0100

    t

--- react-authentication/src/App.js
+++ react-authentication/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar';
+
+
+import Register from './components/register';
+import Login from './components/login';
+import Home from './components/home';
 
 //import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+//Next import Router
 
 class App extends Component {
   render() {
     return (
-        <div>Hello World</div>
+        <div>
+          <Navbar />
+          <Home />
+          <Register />
+          <Login />
+        </div>
     );
   }
 }
